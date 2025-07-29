8ad0b596188af728fefd992c94090ad1745eb64e
malex1306 <alexandremarcel350@gmail.com>
2025-07-29 20:10:04 +0200

    Add MyOrg.HelloWorld module and update solution files

---
+++ src/Smartstore.Modules/MyOrg.HelloWorld/Module.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Smartstore.Engine.Modularity;
+using Smartstore.Http;
+
+internal class Module : ModuleBase, IConfigurable
+{
+    public RouteInfo GetConfigurationRoute()
+        => new("Configure", "HelloWorldAdmin", new { area = "Admin" });
+
+    public override async Task InstallAsync(ModuleInstallationContext context)
+    {
+        await ImportLanguageResourcesAsync();
+
+        await base.InstallAsync(context);
+    }
+
+    public override async Task UninstallAsync()
+    {
+        await DeleteLanguageResourcesAsync();
+
+        await base.UninstallAsync();
+    }
+}
