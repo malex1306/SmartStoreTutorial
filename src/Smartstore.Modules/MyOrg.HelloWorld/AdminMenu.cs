8969997f8988f43f28401132f8ba4e95917a5210
malex1306 <alexandremarcel350@gmail.com>
2025-07-31 09:20:01 +0200

    add MenuItem

---
+++ src/Smartstore.Modules/MyOrg.HelloWorld/AdminMenu.cs
@@ -0,0 +1,36 @@
+﻿using Smartstore.Collections;
+using Smartstore.Core.Content.Menus;
+using Smartstore.Web.Rendering.Builders;
+
+namespace MyOrg.HelloWorld
+{
+    public class AdminMenu : AdminMenuProvider
+    {
+        protected override void BuildMenuCore(TreeNode<MenuItem> modulesNode)
+        {
+            var myMenuItem = new MenuItem().ToBuilder()
+                .ResKey("Plugins.MyOrg.HelloWorld.MyMenuItem")
+                .Icon("gear", "bi")
+                .Action("Configure", "HelloWorldAdmin", new { area = "Admin" })
+                .AsItem();
+
+            var menuNode = new TreeNode<MenuItem>(myMenuItem);
+            var refNode = modulesNode.Root.SelectNodeById("settings");
+            menuNode.InsertAfter(refNode);
+
+            var secondMenuItem = new MenuItem().ToBuilder()
+                .ResKey("Plugins.MyOrg.HelloWorld.MySecondMenuItem")
+                .AsItem();
+            var subMenuItem = new MenuItem().ToBuilder()
+                .ResKey("Plugins.MyOrg.HelloWorld.MySubMenuItem")
+                .Action("Configure", "HelloWorldAdmin", new { area = "Admin" })
+                .AsItem();
+
+            var secondMenuNode = new TreeNode<MenuItem>(secondMenuItem);
+            var subMenuNode = new TreeNode<MenuItem>(subMenuItem);
+
+            secondMenuNode.InsertAfter(menuNode);
+            secondMenuNode.Append(subMenuNode);
+        }
+    }
+}
