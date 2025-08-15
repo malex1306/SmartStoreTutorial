8ca0821f0c39426b72bcea5dfe8e6c2540838000
malex1306 <alexandremarcel350@gmail.com>
2025-08-15 11:06:12 +0200

    fix: resource.en-us.xml

--- src/Smartstore.Modules/MyOrg.HelloWorld/Module.cs
+++ src/Smartstore.Modules/MyOrg.HelloWorld/Module.cs
@@ -1,33 +1,39 @@
+using System.Linq;
+using System.Threading.Tasks;
 using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Smartstore;
 using Smartstore.Core.Content.Menus;
+using Smartstore.Core.Data;
+using Smartstore.Core.DataExchange.Export;
 using Smartstore.Core.Widgets;
+using Smartstore.Engine;
 using Smartstore.Engine.Builders;
 using Smartstore.Engine.Modularity;
 using Smartstore.Http;
-using System.Threading.Tasks;
-using Smartstore.Engine;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Routing;
-using Smartstore.Core.Common;
-using Smartstore.Core.Data;
-using Smartstore.Core.DataExchange.Export;
-using Microsoft.EntityFrameworkCore;
-using System.Linq.Dynamic.Core;
-using Smartstore; // muss ins using damit EachAsync funktioniert
 
 namespace MyOrg.HelloWorld
 {
     public class Module : ModuleBase, IConfigurable, IActivatableWidget, IStarter
     {
-
         private readonly SmartDbContext _db;
         private readonly IExportProfileService _exportProfileService;
 
-        public Module()
+        public Module() { }
+
+        // Abhängigkeiten werden über den Konstruktor injiziert
+        public Module(SmartDbContext db, IExportProfileService exportProfileService)
+        {
+            _db = db;
+            _exportProfileService = exportProfileService;
+        }
+
+        public void ConfigureServices(IServiceCollection services, IApplicationContext appContext)
         {
             
         }
+
         public bool Matches(IApplicationContext appContext) => appContext.IsInstalled;
 
         public void ConfigureContainer(ContainerBuilder builder, IApplicationContext appContext)
@@ -36,7 +42,9 @@ namespace MyOrg.HelloWorld
         }
 
         public RouteInfo GetConfigurationRoute()
-            => new("Configure", "HelloWorldAdmin", new { area = "Admin" });
+        {
+            return new RouteInfo("Configure", "HelloWorldAdmin", new { area = "Admin" });
+        }
 
         public override async Task InstallAsync(ModuleInstallationContext context)
         {
@@ -46,39 +54,34 @@ namespace MyOrg.HelloWorld
 
         public override async Task UninstallAsync()
         {
-            // Dynamic LINQ wird hier verwendet, um mehrere Provider-Systemnamen in einer Abfrage zu filtern
+            // Hier wird der Konstruktor-injizierte Service verwendet
             var profiles = await _db.ExportProfiles
                 .Include(x => x.Deployments)
                 .Include(x => x.Task)
-                .Where("ProviderSystemName == @0 || ProviderSystemName == @1",
-                "MyOrg.HelloWorld.ProductCsv", "MyOrg.HelloWorld.ProductXml").ToListAsync(); 
-            await profiles.EachAsync(x => _exportProfileService.DeleteExportProfileAsync(x, true));
+                .Where(x => x.ProviderSystemName == "MyOrg.HelloWorld.ProductCsv"
+                         || x.ProviderSystemName == "MyOrg.HelloWorld.ProductXml")
+                .ToListAsync();
+
+            foreach (var profile in profiles)
+            {
+                await _exportProfileService.DeleteExportProfileAsync(profile, true);
+            }
+
             await DeleteLanguageResourcesAsync();
             await base.UninstallAsync();
-
-
         }
 
         public Widget GetDisplayWidget(string widgetZone, object model, int storeId) =>
             new ComponentWidget(typeof(HelloWorldViewComponent), new { widgetZone, model, storeId });
 
-        public string[] GetWidgetZones()
-        {
-            return new string[] { "target_widget_zone_name" };
-        }
-
+        public string[] GetWidgetZones() => new string[] { "target_widget_zone_name" };
         public int Order => 0;
 
-        public void ConfigureServices(IServiceCollection services, IApplicationContext appContext) { }
-
         public void ConfigureMvc(IMvcBuilder mvcBuilder, IServiceCollection services, IApplicationContext appContext) { }
-
         public void BuildPipeline(RequestPipelineBuilder builder) { }
-
         public void MapRoutes(EndpointRoutingBuilder builder) { }
 
         public string Key => "MyOrg.HelloWorld";
-
         public string[] DependsOn => new string[0];
     }
 }
